513536e00833f85450ee013192941d8278dbad0f
Andrew Tolsov <andrew.tolstov@gmail.com>
2020-05-31 22:41:35 -0400

    Initial Commit

---
+++ vitata_HW2.4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vitata_HW2.4
+//
+//  Created by Svetlana Tolstova on 5/31/20.
+//  Copyright © 2020 APNET HQ LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
